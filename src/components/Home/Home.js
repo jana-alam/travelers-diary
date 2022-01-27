42435976b70ad6641697d93147bceeae00c16c9b
jana-alam <janaalam2929@gmail.com>
2022-01-27 20:48:59 +0600

    carousel done

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Carousel from "../Carousel/Carousel";
+import Header from "../Header/Header";
+
+function Home() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Carousel />
+    </React.Fragment>
+  );
+}
+
+export default Home;
